2d6ea1242cf1174502c98059ebe4a854da5290b2
Lynh <chinsugaming@gmail.com>
2022-08-26 16:01:43 +0700

    bai1

---
+++ webNangCao_bai1/index.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bai1</title>
+</head>
+
+<body>
+    <style>
+        html {
+            font-family: Arial, Helvetica, sans-serif
+        }
+
+        input {
+            width: auto;
+        }
+        #calc{
+            width: 400px;
+        }
+        #calc.div{
+            width: 100%;
+        }
+    </style>
+    <h2>Xử lý phương trình bậc nhất</h2>
+    <form action="">
+        <!-- number a -->
+        <div id="calc">
+            <div>
+                <label for="a">Số A: </label>
+                <input type="text" id="a" name="a">
+                <br>
+            </div>
+            <!-- number b -->
+            <div>
+                <label for="b">Số B: </label>
+                <input type="text" id="b" name="b">
+                <br>
+            </div>
+            <!-- ket qua -->
+            <div>
+                <label for="ketqua">Kết quả</label>
+                <input type="text" id="ketqua" name="ketqua">
+            </div>
+            <!-- nut bam -->
+            <button type="submit">Ket Qua</button>
+        </div>
+    </form>
+    <?php
+
+    ?>
+</body>
+
+</html>
