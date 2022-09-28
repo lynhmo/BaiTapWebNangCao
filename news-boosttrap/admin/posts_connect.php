86fad792ae6084599bce5853749888e0f98cd940
Lynh <chinsugaming@gmail.com>
2022-09-28 07:42:51 +0700

    bai tap news

---
+++ news-boosttrap/admin/posts_connect.php
@@ -0,0 +1,3 @@
+<?php
+$conn = mysqli_connect("localhost", "root", "rootadmin", "data");
+mysqli_set_charset($conn, 'UTF8');
---
+++ news-boosttrap/connect.php
@@ -0,0 +1,2 @@
+<?php $conn = mysqli_connect("localhost", "root", "rootadmin", "data");
+mysqli_set_charset($conn, 'UTF8');
