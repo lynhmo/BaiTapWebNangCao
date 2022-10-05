16b9d935c399276e5dde70e3dbc6bd6f34d256a9
Lynh <chinsugaming@gmail.com>
2022-10-05 11:35:41 +0700

    bai tap news yes

--- register/main.php
+++ register/main.php
@@ -5,40 +5,11 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../node_modules/bootstrap/dist/css/bootstrap.min.css">
-    <link rel="stylesheet" href="./assets/css/main.css">
     <title>Document</title>
 </head>
 
 <body>
-    <script src="../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
-    <!-- header -->
-    <div class="head">
-        <!-- container -->
-        <div class="container">
-            <!-- Example single danger button -->
-            <div class="btn-group">
-                <button type="button" class="btn btn-danger dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
-                    Action
-                </button>
-                <ul class="dropdown-menu">
-                    <li><a class="dropdown-item" href="#">Action</a></li>
-                    <li><a class="dropdown-item" href="#">Another action</a></li>
-                    <li><a class="dropdown-item" href="#">Something else here</a></li>
-                    <li>
-                        <hr class="dropdown-divider">
-                    </li>
-                    <li><a class="dropdown-item" href="#">Separated link</a></li>
-                </ul>
-            </div>
-        </div>
-    </div>
-    <!-- body -->
-    <div></div>
-    <!-- footer -->
-    <footer>
-
-    </footer>
+    <h1>Trang chu</h1>
 </body>
 
 </html>
